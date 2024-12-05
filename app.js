649ce3957b832716aaac6771e2108b1332885848
Samuel Xavier <sancrisxa@gmail.com>
2024-12-04 23:39:45 -0300

    alterando o numero de tentativas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+let tentativas = 3;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
